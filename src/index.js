4a1f6ec474599fa4fad0ec16a9d71679298f22e7
Yogaravi <yogaravi666@gmail.com>
2024-03-29 21:08:22 +0530

    updated all files

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import reactdom from "react-dom/client"
+import one from "./task.jpg"
+const root = reactdom.createRoot(document.getElementById("root"))
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function Image() 
+{
+  return(<div>
+    <img src={one}alt="one"></img>
+    <h2>jullie rabit ears</h2>
+
+    </div>
+    
+  )  
+}
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  <Image> </Image>
+)
